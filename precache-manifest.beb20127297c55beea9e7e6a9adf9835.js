1989f1ecd8a5c610709b4bdbfe2be58c54bf13d6
Henry Chung <mc17ac@my.fsu.edu>
2020-08-17 14:32:30 -0400

    Updates

---
+++ precache-manifest.beb20127297c55beea9e7e6a9adf9835.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f447aa314f050ba00957bf8159c2c88d",
+    "url": "/holiday/index.html"
+  },
+  {
+    "revision": "8cc1703d69f5e51d0e1c",
+    "url": "/holiday/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "9b6fb6ca85c790a3de5b",
+    "url": "/holiday/static/css/main.110f8478.chunk.css"
+  },
+  {
+    "revision": "8cc1703d69f5e51d0e1c",
+    "url": "/holiday/static/js/2.611f6828.chunk.js"
+  },
+  {
+    "revision": "9b6fb6ca85c790a3de5b",
+    "url": "/holiday/static/js/main.7cb163ec.chunk.js"
+  },
+  {
+    "revision": "db4f7752a5a4abf45cd4",
+    "url": "/holiday/static/js/runtime~main.27729716.js"
+  },
+  {
+    "revision": "6225300ce5e0e81fa29b82adb4717cb1",
+    "url": "/holiday/static/media/fsulogo.6225300c.png"
+  }
+]);
